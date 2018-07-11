a12fe5eb944a1a09ce02ecf4e7bac928fb9c7311
Rachel Canda <rachel@screenmedia.co.uk>
2018-07-11 16:23:14 +0100

    working on help screen

--- TravelBuddy/TravelBuddy/GoToGate.xaml.cs
+++ TravelBuddy/TravelBuddy/GoToGate.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Plugin.TextToSpeech;
+using Plugin.Vibrate;
 using Xamarin.Forms;
 
 namespace TravelBuddy
@@ -16,7 +18,18 @@ namespace TravelBuddy
         {
             base.OnAppearing();
 
-            CrossTextToSpeech.Current.Speak("Text to speak");
+            OnStart();
+        }
+
+        private async Task OnStart()
+        {
+            await CrossTextToSpeech.Current.Speak("We have your current location, stay where you are and help will be with you soon.");
+
+            await Task.Delay(2500);
+
+            CrossVibrate.Current.Vibration(TimeSpan.FromSeconds(1));
+
+            await CrossTextToSpeech.Current.Speak("Help has arrived.");
         }
     }
 }
