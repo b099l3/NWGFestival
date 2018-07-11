51c0b4b051f8ae2a97517c71f171e422485de04b
Rachel Canda <rachel@screenmedia.co.uk>
2018-07-11 17:15:41 +0100

    add solo traveller page

---
+++ TravelBuddy/TravelBuddy/SoloTravellerPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TravelBuddy
+{
+    public partial class SoloTravellerPage : ContentPage
+    {
+        public SoloTravellerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
