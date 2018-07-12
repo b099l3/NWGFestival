70d87c7bb5ff53785d1ccc49ecef8fb394b791a3
Iain Smith <b099l3@gmail.com>
2018-07-12 10:12:49 +0100

    iain smith

--- TravelBuddy/TravelBuddy/ShopsPage.xaml.cs
+++ TravelBuddy/TravelBuddy/ShopsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using Plugin.TextToSpeech;
 using Xamarin.Forms;
 
 namespace TravelBuddy
@@ -10,6 +11,37 @@ namespace TravelBuddy
         public ShopsPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await OnStart();
+        }
+
+        private async Task OnStart()
+        {
+            await CrossTextToSpeech.Current.Speak("We have found 3 shops near your location: WHSmith, Boots Pharmacy and Starbucks.");
+        }
+
+        async void OnBackButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void OnWHSButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void OnBootsButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void OnStarbucksButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }
